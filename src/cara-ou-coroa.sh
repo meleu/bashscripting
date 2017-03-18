2a7206bd72d2d140dfbb59c2123670b38682b1b9
meleu <meleu@users.noreply.github.com>
2017-03-18 02:31:01 -0300

    Update cara-ou-coroa.sh

--- src/cara-ou-coroa.sh
+++ src/cara-ou-coroa.sh
@@ -2,5 +2,5 @@
 # cara-ou-coroa.sh
 # crédito do código: Matheus Alves - https://github.com/theuves
 
-# se o número aleatório é par, temos "Cara", se for ímpar temos Coroa
+# se o número aleatório é par, temos "Cara", se for ímpar temos "Coroa"
 [ $[ $RANDOM % 2 ] = 0 ] && echo "Cara" || echo "Coroa"
