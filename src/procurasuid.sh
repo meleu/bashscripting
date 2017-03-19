7cdf9fe157b12f3376756479c6bb2a2e42fc0a1d
meleu <meleu@users.noreply.github.com>
2017-03-18 22:16:10 -0300

    Update procurasuid.sh

--- src/procurasuid.sh
+++ src/procurasuid.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # procurasuid.sh
-# script para procurar arquivos suid
-# que pertençam a determinado usuário
+# script para procurar arquivos suid que pertençam a determinado usuário
 find / -user $1 -perm -4000 2> /dev/null
